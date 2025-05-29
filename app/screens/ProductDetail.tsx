e18d45c06805371b8b0cbe32ff059c56121b471c
Romil Movaliya <21amtics247@gmail.com>
2025-05-29 11:53:16 +0530

    complete header section with title and description.

---
+++ app/screens/ProductDetail.tsx
@@ -0,0 +1,73 @@
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import BackIcon from "../../assets/backIcon.svg";
+import ShareIcon from "../../assets/shareIcon.svg";
+import FavoriteIcon from "../../assets/tabIcons/favoriteIcon.svg";
+import { router } from 'expo-router';
+const ProductDetail = () => {
+    return (
+        <SafeAreaView>
+
+            <View style={styles.topBox}>
+                <Image
+                    style={{
+                        width: '100%',
+                        borderColor: 'red'
+                    }}
+                    resizeMode='contain'
+                    source={require('../../assets/appleIcon.png')}
+                />
+            </View>
+
+            <View style={styles.topheading}>
+                <BackIcon height={24} width={24} onPress={() => { router.back() }} />
+                <ShareIcon height={25} width={25} />
+            </View>
+
+            <View>
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: 20,
+                    paddingTop: 10
+                }}>
+                    <Text>Natural Red Apple</Text>
+                    <FavoriteIcon width={24} height={24} />
+                </View>
+
+                <Text style={{
+                    color: '#7C7C7C',
+                    paddingHorizontal: 20,
+                }}>1kg, Price</Text>
+            </View>
+
+
+
+        </SafeAreaView>
+    )
+}
+
+export default ProductDetail
+
+const styles = StyleSheet.create({
+    topheading: {
+        position: 'absolute',
+        width: '100%',
+        flexDirection: 'row',
+        paddingHorizontal: 25,
+        paddingVertical: 30,
+        justifyContent: 'space-between'
+    },
+    topBox: {
+        alignSelf: 'center',
+        width: '100%',
+        borderWidth: 2,
+        borderTopWidth: 0,
+        borderColor: '#E2E3E2',
+        height: 300,
+        marginTop: 80,
+        justifyContent: 'center',
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25
+    }
+})
