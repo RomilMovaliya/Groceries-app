5834fe365554d1ce0a8d9a5630cee4a3c4f0bb07
Romil Movaliya <105592033+RomilMovaliya@users.noreply.github.com>
2025-06-26 14:01:24 +0530

    optimizing auth api and setup in related file (#40)

--- app/screens/AccountScreen.tsx
+++ app/screens/AccountScreen.tsx
@@ -1,5 +1,14 @@
-import { FlatList, Image, Modal, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import {
+  FlatList,
+  Image,
+  Modal,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import OrderIcon from "../../assets/ProfileIcons/OrdersIcon.svg";
 import MyDetailIcon from "../../assets/ProfileIcons/MyDetailsIcon.svg";
 import DeliveryAddressIcon from "../../assets/ProfileIcons/DeliceryAddress.svg";
@@ -9,271 +18,286 @@ import NotificationIcon from "../../assets/ProfileIcons/BellIcon.svg";
 import HelpIcon from "../../assets/ProfileIcons/helpIcon.svg";
 import AboutIcon from "../../assets/ProfileIcons/aboutIcon.svg";
 import LeftIcon from "../../assets/leftColorIcon.svg";
-import Button from '../../components/button';
-import LogoutIcon from '../../assets/ProfileIcons/LogoutIcon.svg';
-import { primaryColor } from '../../utils/myColors';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { router } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { getUserData, logout } from '../../supabase/auth/authFunction';
-import { supabase } from '../../supabase/supabase';
+import Button from "../../components/button";
+import LogoutIcon from "../../assets/ProfileIcons/LogoutIcon.svg";
+import { primaryColor } from "../../utils/myColors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { getUserSession, logout } from "../../supabase/auth/authFunction";
+import Toast from "react-native-toast-message";
 
 const AccountScreen = () => {
-
-    const [name, setName] = useState("userName");
-    const [email, setEmail] = useState("youmail@gmail.com");
-    const [uri, setUri] = useState('');
-    const [modeVisible, setModeVisible] = useState(false);
-    const getUserData1 = async () => {
-        const imageUri = await AsyncStorage.getItem('capturedPhoto');
-        if (imageUri) {
-            setUri(imageUri);
-        }
+  const [name, setName] = useState("userName");
+  const [email, setEmail] = useState("youmail@gmail.com");
+  const [uri, setUri] = useState("");
+  const [modeVisible, setModeVisible] = useState(false);
+  const getUserData1 = async () => {
+    const imageUri = await AsyncStorage.getItem("capturedPhoto");
+    if (imageUri) {
+      setUri(imageUri);
     }
+  };
 
-    useEffect(() => {
-        const fetchUserInfo = async () => {
-            const userInfo = await getUserData();
-            setName(userInfo.data.session.user.user_metadata.display_name);
-            setEmail(userInfo.data.session.user.user_metadata.email)
-            console.log("userInfo", userInfo.data.session.user.user_metadata.display_name);
-        };
-        fetchUserInfo();
-        getUserData1();
-    }, [])
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const userInfo = await getUserSession();
+      if (userInfo.success) {
+        setName(userInfo.user.session.user.user_metadata.display_name);
+        setEmail(userInfo.user.session.user.user_metadata.email);
+        console.log(
+          "userInfo",
+          userInfo.user.session.user.user_metadata.display_name
+        );
+      }
+    };
+    fetchUserInfo();
+    getUserData1();
+  }, []);
 
-    const handleLogout = async () => {
-        await supabase.auth.signOut();
-        router.dismissTo("/screens/Login");
+  const handleLogout = async () => {
+    const logoutstatus = await logout();
+    if (logoutstatus.success) {
+      router.dismissTo("/screens/Login");
+    } else {
+      Toast.show({
+        type: "error",
+        text1: "Signup Failed",
+        text2: logoutstatus.message,
+      });
     }
+  };
 
-    const handleModeToggle = () => {
-        setModeVisible(!modeVisible);
-    }
-    const profileOptions = [
-        {
-            id: 1,
-            icon: OrderIcon,
-            title: 'Orders'
-        },
-        {
-            id: 2,
-            icon: MyDetailIcon,
-            title: 'My Details'
-        },
-        {
-            id: 3,
-            icon: DeliveryAddressIcon,
-            title: 'Delivery Address'
-        },
-        {
-            id: 4,
-            icon: PaymentIcon,
-            title: 'Payment Methods'
-        },
-        {
-            id: 5,
-            icon: PromocodeIcon,
-            title: 'Promo Cord'
-        },
-        {
-            id: 6,
-            icon: NotificationIcon,
-            title: 'Notifications'
-        },
-        {
-            id: 7,
-            icon: HelpIcon,
-            title: 'Help'
-        },
-        {
-            id: 8,
-            icon: AboutIcon,
-            title: 'About'
-        }
-    ];
+  const handleModeToggle = () => {
+    setModeVisible(!modeVisible);
+  };
+  const profileOptions = [
+    {
+      id: 1,
+      icon: OrderIcon,
+      title: "Orders",
+    },
+    {
+      id: 2,
+      icon: MyDetailIcon,
+      title: "My Details",
+    },
+    {
+      id: 3,
+      icon: DeliveryAddressIcon,
+      title: "Delivery Address",
+    },
+    {
+      id: 4,
+      icon: PaymentIcon,
+      title: "Payment Methods",
+    },
+    {
+      id: 5,
+      icon: PromocodeIcon,
+      title: "Promo Cord",
+    },
+    {
+      id: 6,
+      icon: NotificationIcon,
+      title: "Notifications",
+    },
+    {
+      id: 7,
+      icon: HelpIcon,
+      title: "Help",
+    },
+    {
+      id: 8,
+      icon: AboutIcon,
+      title: "About",
+    },
+  ];
 
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar backgroundColor={"white"} />
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <StatusBar backgroundColor={'white'} />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-between",
+        }}
+      >
+        <View style={styles.header}>
+          {modeVisible && (
+            <Modal visible={true} transparent={true} animationType="fade">
+              <TouchableOpacity
+                activeOpacity={1}
+                onPressOut={() => setModeVisible(false)}
+                style={styles.modalOverlay}
+              >
+                <TouchableOpacity
+                  activeOpacity={1}
+                  style={styles.modalContent}
+                  onPress={() => {}}
+                >
+                  <TouchableOpacity
+                    onPress={() => {
+                      setModeVisible(false);
+                      router.navigate("/screens/ImageFromGallary");
+                    }}
+                  >
+                    <Text style={styles.modalText}>Select from Gallery</Text>
+                  </TouchableOpacity>
 
-            <View style={{
-                flex: 1,
-                justifyContent: 'space-between'
-            }}>
-                <View style={styles.header}>
+                  <View style={styles.modalDivider} />
 
-                    {modeVisible && (
-                        <Modal visible={true} transparent={true} animationType="fade">
-                            <TouchableOpacity
-                                activeOpacity={1}
-                                onPressOut={() => setModeVisible(false)}
-                                style={styles.modalOverlay}
-                            >
-                                <TouchableOpacity
-                                    activeOpacity={1}
-                                    style={styles.modalContent}
-                                    onPress={() => { }}
-                                >
-                                    <TouchableOpacity
-                                        onPress={() => {
-                                            setModeVisible(false);
-                                            router.navigate('/screens/ImageFromGallary');
-                                        }}
-                                    >
-                                        <Text style={styles.modalText}>Select from Gallery</Text>
-                                    </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setModeVisible(false);
+                      router.navigate("/screens/CameraScreen");
+                    }}
+                  >
+                    <Text style={styles.modalText}>Select from Camera</Text>
+                  </TouchableOpacity>
+                </TouchableOpacity>
+              </TouchableOpacity>
+            </Modal>
+          )}
 
-                                    <View style={styles.modalDivider} />
+          <TouchableOpacity
+            onPress={() => {
+              setModeVisible(!modeVisible);
+            }}
+          >
+            <Image
+              style={{
+                height: 80,
+                width: 80,
+                borderRadius: 100,
+                borderWidth: 0.1,
+              }}
+              source={{ uri: uri }}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+          <View>
+            <Text>{name}</Text>
 
-                                    <TouchableOpacity
-                                        onPress={() => {
-                                            setModeVisible(false);
-                                            router.navigate('/screens/CameraScreen');
-                                        }}
-                                    >
-                                        <Text style={styles.modalText}>Select from Camera</Text>
-                                    </TouchableOpacity>
-                                </TouchableOpacity>
-                            </TouchableOpacity>
-                        </Modal>
-                    )}
+            <Text
+              style={{
+                color: "grey",
+              }}
+            >
+              {email}
+            </Text>
+          </View>
+        </View>
 
+        <FlatList
+          keyExtractor={(item) => item.id.toString()}
+          data={profileOptions}
+          renderItem={({ item }) => (
+            <View style={styles.listitem}>
+              <View style={styles.leftlistitem}>
+                <item.icon />
+                <Text>{item.title}</Text>
+              </View>
 
-
-
-                    <TouchableOpacity onPress={() => {
-                        setModeVisible(!modeVisible);
-                    }}>
-                        <Image
-                            style={{
-                                height: 80,
-                                width: 80,
-                                borderRadius: 100,
-                                borderWidth: 0.1,
-                            }}
-                            source={{ uri: uri }}
-                            resizeMode="contain"
-                        />
-
-                    </TouchableOpacity>
-                    <View>
-                        <Text>{name}</Text>
-
-                        <Text style={{
-                            color: 'grey'
-                        }}>{email}</Text>
-                    </View>
-                </View>
-
-                <FlatList
-                    keyExtractor={(item) => item.id.toString()}
-                    data={profileOptions}
-                    renderItem={({ item }) => (
-                        <View style={styles.listitem}>
-                            <View style={styles.leftlistitem}>
-                                <item.icon />
-                                <Text>{item.title}</Text>
-                            </View>
-
-                            <View>
-                                <LeftIcon width={20} height={20} />
-                            </View>
-                        </View>
-                    )}
-                />
-
-                <View style={styles.btn}>
-                    <Button
-                        style={styles.logout}
-                        title='Log Out'
-                        onPress={handleLogout}
-                        textStyle={{ color: primaryColor, backgroundColor: '#ebebeb' }}
-                    />
-                    <LogoutIcon style={{
-                        position: 'absolute',
-                        left: 20,
-                        top: 25
-                    }} height={20} width={20} />
-
-                </View>
+              <View>
+                <LeftIcon width={20} height={20} />
+              </View>
             </View>
+          )}
+        />
 
-        </SafeAreaView>
-    )
-}
+        <View style={styles.btn}>
+          <Button
+            style={styles.logout}
+            title="Log Out"
+            onPress={handleLogout}
+            textStyle={{ color: primaryColor, backgroundColor: "#ebebeb" }}
+          />
+          <LogoutIcon
+            style={{
+              position: "absolute",
+              left: 20,
+              top: 25,
+            }}
+            height={20}
+            width={20}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default AccountScreen
+export default AccountScreen;
 
 const styles = StyleSheet.create({
-    header: {
-        padding: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 20,
-        borderBottomWidth: 1.5,
-        borderBottomColor: '#E2E2E2'
-    },
+  header: {
+    padding: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+    borderBottomWidth: 1.5,
+    borderBottomColor: "#E2E2E2",
+  },
 
-    listitem: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        borderBottomColor: '#E2E2E2',
-        borderBottomWidth: 1.5,
-        paddingVertical: 15,
-        marginHorizontal: 20
-    },
+  listitem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "#E2E2E2",
+    borderBottomWidth: 1.5,
+    paddingVertical: 15,
+    marginHorizontal: 20,
+  },
 
-    leftlistitem: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 10
-    },
-    btn: {
-        marginHorizontal: 10,
-        marginVertical: 10
-    },
-    logout: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        textAlign: 'center',
-        backgroundColor: '#ebebeb',
-        padding: 10,
-        justifyContent: 'center',
-        borderRadius: 15
-    },
+  leftlistitem: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  btn: {
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  logout: {
+    flexDirection: "row",
+    alignItems: "center",
+    textAlign: "center",
+    backgroundColor: "#ebebeb",
+    padding: 10,
+    justifyContent: "center",
+    borderRadius: 15,
+  },
 
-    modalOverlay: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
 
-    modalContent: {
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
-        width: '80%',
-        elevation: 5, // For Android shadow
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-    },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    width: "80%",
+    elevation: 5, // For Android shadow
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
 
-    modalText: {
-        fontWeight: 'bold',
-        fontSize: 16,
-        marginBottom: 10,
-    },
+  modalText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 10,
+  },
 
-    modalDivider: {
-        height: 1,
-        backgroundColor: '#ccc',
-        marginVertical: 10,
-    }
-
-})
+  modalDivider: {
+    height: 1,
+    backgroundColor: "#ccc",
+    marginVertical: 10,
+  },
+});
--- supabase/auth/authFunction.ts
+++ supabase/auth/authFunction.ts
@@ -1,135 +1,170 @@
-import { Alert } from "react-native";
 import { supabase } from "../../supabase/supabase";
 
 export const checkUserExists = async (email: string) => {
-    const { data, error } = await supabase.rpc('check_user_exists', {
-        user_email: email
+  try {
+    const { data, error } = await supabase.rpc("check_user_exists", {
+      user_email: email,
     });
 
     if (error) {
-        throw error;
+      throw error;
     }
 
     return data;
-}
+  } catch (error) {
+    console.log("error", error);
+    return { success: false, message: error.message };
+  }
+};
 
-
-export const signUpWithEmail = async (username: string, email: string, password: string) => {
-    try {
-
-        const existingUser = await checkUserExists(email);
-        if (existingUser) {
-            throw Error("user already exits")
-        }
-
-        const { data, error } = await supabase.auth.signUp({
-            email: email,
-            password: password,
-            options: {
-                data: {
-                    display_name: username
-                }
-            }
-        })
-
-        if (error) {
-            console.log(error.message);
-            return { success: false, message: error.message };
-        }
-
-        if (data) {
-            return { success: true, message: null }
-        }
-    } catch (error) {
-        console.log("error", error);
-        return { success: false, message: error.message }
+export const signUpWithEmail = async (
+  username: string,
+  email: string,
+  password: string
+) => {
+  try {
+    const existingUser = await checkUserExists(email);
+    if (existingUser) {
+      throw Error("user already exits");
     }
 
-}
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        data: {
+          display_name: username,
+        },
+      },
+    });
+
+    if (error) {
+      console.log(error.message);
+      return { success: false, message: error.message };
+    }
+
+    if (data) {
+      return { success: true, message: null };
+    }
+  } catch (error) {
+    console.log("error", error);
+    return { success: false, message: error.message };
+  }
+};
 
 export const resendOtp = async (email: string) => {
+  try {
     const { data, error } = await supabase.auth.resend({
-        type: 'signup',
-        email
+      type: "signup",
+      email,
     });
 
     console.log("data", data);
 
     if (error) {
-        return { suceess: false, message: error.message }
+      return { suceess: false, message: error.message };
     }
 
-    return { success: true, message: "otp resended successfully." }
-}
-
+    return { success: true, message: "otp resended successfully." };
+  } catch (error) {
+    console.log("error", error);
+    return { success: false, message: error.message };
+  }
+};
 
 export const signInWithEmail = async (email: string, password: string) => {
-    const { data: { session }, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password
+  try {
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
     });
     if (error) {
-        console.log("Sign-in failed:", error.message);
-        return { success: false, error: error.message };
+      console.log("Sign-in failed:", error.message);
+      return { success: false, error: error.message };
     }
 
     if (session) {
-        console.log("Sign-in successful for:", email);
-        return { success: true };
+      console.log("Sign-in successful for:", email);
+      return { success: true };
     } else {
-        console.warn("Sign-in attempt returned no session.");
-        return { success: false, error: "No session returned." };
+      console.warn("Sign-in attempt returned no session.");
+      return { success: false, error: "No session returned." };
     }
-}
+  } catch (error) {
+    console.log("error", error);
+    return { success: false, message: error.message };
+  }
+};
 
-export const getUserData = async () => {
-    const user = await supabase.auth.getSession();
-    return user;
-}
+export const getUserSession = async () => {
+  try {
+    const { data: user, error } = await supabase.auth.getSession();
+    if (error) {
+      console.log("error in getuser", error.message);
+      return { success: false, message: error.message };
+    }
+    return { success: true, message: "data getting successfully.", user };
+  } catch (error) {
+    console.log("error in catch", error.message);
+    return { success: false, message: error.message };
+  }
+};
 
 export const logout = async () => {
-    await supabase.auth.signOut();
-}
+  try {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.log("Error during logout", error.message);
+      return { success: false, message: error.message };
+    }
+    return { success: true, message:"logout successful." };
+  } catch (error) {
+    console.log("Error in catch", error.message);
+    return { success: false, message: error.message };
+  }
+};
 
 export const signInWithOtpFunc = async (email: string) => {
-    try {
-        const { data, error } = await supabase.auth.signInWithOtp({
-            email: email,
-            options: {
-                shouldCreateUser: false, // Don't create user if they don't exist
-            },
-        });
-        if (error) {
-            Alert.alert('OTP Error', error.message);
-            console.log('OTP error:', error);
-            return { success: false, error };
-        }
-        console.log('OTP sent successfully:', data);
-        return { success: true, data };
-    } catch (error) {
-        console.error('OTP error:', error);
-        Alert.alert('OTP Error', 'An unexpected error occurred');
-        return { success: false, error };
+  try {
+    const { data, error } = await supabase.auth.signInWithOtp({
+      email: email,
+      options: {
+        shouldCreateUser: false,
+      },
+    });
+    if (error) {
+      console.log("OTP error:", error);
+      return { success: false, error };
     }
+    console.log("OTP sent successfully:", data);
+    return { success: true, data };
+  } catch (error) {
+    console.error("OTP error:", error);
+    return { success: false, error };
+  }
 };
 
 export const verifyOTP = async (email: string, otp: string) => {
+  try {
     const { data, error } = await supabase.auth.verifyOtp({
-        email,
-        token: otp,
-        type: 'email',
+      email,
+      token: otp,
+      type: "email",
     });
 
-    console.log("data", data);
-    console.log("error", error);
-
     if (error) {
-        console.log("error in verifyOTP", error);
-        return { success: false, messsage: error.message }
+      console.log("error in verifyOTP", error);
+      return { success: false, messsage: error.message };
     }
     if (data) {
-        console.log("otp is verifed");
-        return { success: true, message: data };
+      console.log("otp is verifed");
+      return { success: true, message: data };
     }
-}
-
+  } catch (error) {
+    console.error("OTP error:", error.message);
+    return { success: false, message: error.messsage };
+  }
+};
